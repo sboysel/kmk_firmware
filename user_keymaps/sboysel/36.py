1d71ffceb093dda1e9057a29670e29d1016eadee
Sam Boysel <sboysel@gmail.com>
2023-02-24 16:02:23 -0500

    adding simplified miryoku keymap

--- user_keymaps/sboysel/36.py
+++ user_keymaps/sboysel/36.py
@@ -10,29 +10,31 @@ from kmk.modules.tapdance import TapDance; keyboard.modules.append(TapDance())
 from kmk.extensions.media_keys import MediaKeys; keyboard.extensions.append(MediaKeys())
 from kmk.modules.capsword import CapsWord; keyboard.modules.append(CapsWord())
 
+tap_time = 200
+
 # === layer tap ---------------------------------------------------------------
 # NAV (layer 1)
-LT_TAB = KC.LT(1, KC.TAB, prefer_hold=True, tap_interrupted=False, tap_time=200)
-LT_SPC = KC.LT(1, KC.SPC, prefer_hold=True, tap_interrupted=False, tap_time=200)
+LT_TAB = KC.LT(1, KC.TAB, prefer_hold=True, tap_interrupted=False, tap_time=tap_time)
+LT_SPC = KC.LT(1, KC.SPC, prefer_hold=True, tap_interrupted=False, tap_time=tap_time)
 # NUM (layer 2)
-LT_ENT = KC.LT(2, KC.ENT, prefer_hold=True, tap_interrupted=False, tap_time=200)
+LT_BSPC = KC.LT(2, KC.BSPC, prefer_hold=True, tap_interrupted=False, tap_time=tap_time)
 # SYM (layer 3)
-LT_BSPC = KC.LT(3, KC.BSPC, prefer_hold=True, tap_interrupted=False, tap_time=200)
+LT_ENT = KC.LT(3, KC.ENT, prefer_hold=True, tap_interrupted=False, tap_time=tap_time)
 # FUN (layer 4)
-LT_DEL = KC.LT(4, KC.DEL, prefer_hold=True, tap_interrupted=False, tap_time=200)
+LT_DEL = KC.LT(4, KC.DEL, prefer_hold=True, tap_interrupted=False, tap_time=tap_time)
 # MEDIA (layer 5)
-LT_ESC = KC.LT(5, KC.ESC, prefer_hold=True, tap_interrupted=False, tap_time=200)
+LT_ESC = KC.LT(5, KC.ESC, prefer_hold=True, tap_interrupted=False, tap_time=tap_time)
 
 # === home row mods ------------------------------------------------------------
-MT_A = KC.MT(KC.A, KC.LALT, prefer_hold=False, tap_interrupted=True, tap_time=200)
-MT_S = KC.MT(KC.S, KC.LCTL, prefer_hold=False, tap_interrupted=True, tap_time=200)
-MT_D = KC.MT(KC.D, KC.LGUI, prefer_hold=False, tap_interrupted=True, tap_time=200)
-MT_F = KC.MT(KC.F, KC.LSFT, prefer_hold=False, tap_interrupted=True, tap_time=200)
+MT_A = KC.MT(KC.A, KC.LGUI, prefer_hold=False, tap_interrupted=True, tap_time=tap_time)
+MT_S = KC.MT(KC.S, KC.LALT, prefer_hold=False, tap_interrupted=True, tap_time=tap_time)
+MT_D = KC.MT(KC.D, KC.LCTL, prefer_hold=False, tap_interrupted=True, tap_time=tap_time)
+MT_F = KC.MT(KC.F, KC.LSFT, prefer_hold=False, tap_interrupted=True, tap_time=tap_time)
 
-MT_J = KC.MT(KC.J, KC.LSFT, prefer_hold=False, tap_interrupted=True, tap_time=200)
-MT_K = KC.MT(KC.K, KC.LGUI, prefer_hold=False, tap_interrupted=True, tap_time=200)
-MT_L = KC.MT(KC.L, KC.LCTL, prefer_hold=False, tap_interrupted=True, tap_time=200)
-MT_QUOT = KC.MT(KC.QUOT, KC.LALT, prefer_hold=False, tap_interrupted=True, tap_time=200)
+MT_J = KC.MT(KC.J, KC.LSFT, prefer_hold=False, tap_interrupted=True, tap_time=tap_time)
+MT_K = KC.MT(KC.K, KC.LCTL, prefer_hold=False, tap_interrupted=True, tap_time=tap_time)
+MT_L = KC.MT(KC.L, KC.LALT, prefer_hold=False, tap_interrupted=True, tap_time=tap_time)
+MT_QUOT = KC.MT(KC.QUOT, KC.LGUI, prefer_hold=False, tap_interrupted=True, tap_time=tap_time)
 
 # === keymap -------------------------------------------------------------------
 keyboard.keymap = [
@@ -47,35 +49,35 @@ keyboard.keymap = [
     # NAV
     [
         KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,      KC.NO,  KC.NO,   KC.NO,   KC.NO,   KC.NO,
-        KC.LALT, KC.LCTL, KC.LGUI, KC.LSFT, KC.NO,      KC.NO,  KC.LEFT, KC.DOWN, KC.UP,   KC.RGHT,
+        KC.LGUI, KC.LALT, KC.LCTL, KC.LSFT, KC.NO,      KC.NO,  KC.LEFT, KC.DOWN, KC.UP,   KC.RGHT,
         KC.NO,   KC.RALT, KC.NO,   KC.NO,   KC.NO,      KC.INS, KC.HOME, KC.PGDN, KC.PGUP, KC.END,
         KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,      KC.ENT, KC.BSPC, KC.DEL,  KC.NO,   KC.NO
     ],
     # NUM
     [
         KC.LBRC, KC.N7,   KC.N8,   KC.N9,   KC.RBRC,    KC.NO,  KC.NO,   KC.NO,   KC.NO,   KC.NO,
-        KC.SCLN, KC.N4,   KC.N5,   KC.N6,   KC.EQL,     KC.NO,  KC.LSFT, KC.LGUI, KC.LCTL, KC.LALT,
+        KC.SCLN, KC.N4,   KC.N5,   KC.N6,   KC.EQL,     KC.NO,  KC.LSFT, KC.LCTL, KC.LALT, KC.LGUI,
         KC.GRV,  KC.N1,   KC.N2,   KC.N3,   KC.BSLS,    KC.NO,  KC.NO,   KC.NO,   KC.RALT, KC.NO,
         KC.NO,   KC.NO,   KC.DOT,  KC.N0,   KC.MINS,    KC.NO,  KC.NO,   KC.NO,   KC.NO,   KC.NO
     ],
     # SYM
     [
         KC.LCBR, KC.AMPR, KC.ASTR, KC.LPRN, KC.RCBR,    KC.NO,  KC.NO,   KC.NO,   KC.NO,   KC.NO,
-        KC.COLN, KC.DLR,  KC.PERC, KC.CIRC, KC.PLUS,    KC.NO,  KC.LSFT, KC.LGUI, KC.LCTL, KC.LALT,
+        KC.COLN, KC.DLR,  KC.PERC, KC.CIRC, KC.PLUS,    KC.NO,  KC.LSFT, KC.LCTL, KC.LALT, KC.LGUI,
         KC.TILD, KC.EXLM, KC.AT,   KC.HASH, KC.PIPE,    KC.NO,  KC.NO,   KC.NO,   KC.RALT, KC.NO,
         KC.NO,   KC.NO,   KC.LPRN, KC.RPRN, KC.UNDS,    KC.NO,  KC.NO,   KC.NO,   KC.NO,   KC.NO
     ],
     # FUN
     [
         KC.F12,  KC.F7,   KC.F8,   KC.F9,   KC.PSCR,    KC.NO,  KC.NO,   KC.NO,   KC.NO,   KC.NO,
-        KC.F11,  KC.F4,   KC.F5,   KC.F6,   KC.SLCK,    KC.NO,  KC.LSFT, KC.LGUI, KC.LCTL, KC.LALT,
+        KC.F11,  KC.F4,   KC.F5,   KC.F6,   KC.SLCK,    KC.NO,  KC.LSFT, KC.LCTL, KC.LALT, KC.LGUI,
         KC.F10,  KC.F1,   KC.F2,   KC.F3,   KC.PAUS,    KC.NO,  KC.NO,   KC.NO,   KC.RALT, KC.NO,
         KC.NO,   KC.NO,   KC.APP,  KC.SPC,  KC.TAB,     KC.NO,  KC.NO,   KC.NO,   KC.NO,   KC.NO
     ],
     # MEDIA
     [
         KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,      KC.NO,     KC.NO,   KC.NO,   KC.NO,   KC.NO,
-        KC.LALT, KC.LCTL, KC.LGUI, KC.LSFT, KC.NO,      KC.PS_TOG, KC.MPRV, KC.VOLD, KC.VOLU, KC.MNXT,
+        KC.LGUI, KC.LALT, KC.LCTL, KC.LSFT, KC.NO,      KC.PS_TOG, KC.MPRV, KC.VOLD, KC.VOLU, KC.MNXT,
         KC.NO,   KC.RALT, KC.NO,   KC.NO,   KC.NO,      KC.HID,    KC.NO,   KC.NO,   KC.NO,   KC.NO,
         KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,      KC.MSTP,   KC.MPLY, KC.MUTE, KC.NO,   KC.NO
     ],
@@ -86,6 +88,4 @@ layer_names_list = [
 ]
 
 if __name__ == '__main__':
-    print('board:   CrowBoard')
-    print('keymap:  Miryoku KMK')
     keyboard.go()
